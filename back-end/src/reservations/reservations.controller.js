bc2249cc1f1e5a518e73ebac9b614478e675f704
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-02-03 11:22:52 -0800

    US 1 backend: GET / reservations
    ✓ returns only reservations matching date query parameter
    ✓ returns reservations sorted by time (earliest time first)

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,12 +4,20 @@ const service = require("./reservations.service");
 /**
  * List handler for reservation resources
  */
+
 async function list(req, res) {
   let data = await service.list()
-
-  res.json({ data });
+  
+  data = data
+    .filter((item)=>{ 
+      return item.reservation_date == req.query.date
+    })
+    .sort((a, b) => a.reservation_time.slice(0,1) - b.reservation_time.slice(0,1))
+    .sort((a, b) => a.reservation_time.slice(3,4) - b.reservation_time.slice(3,4))
+  console.log(data)
+  res.json({ data })
 }
 
 module.exports = {
-  list: asyncErrorBoundary(list) ,
+  list: [asyncErrorBoundary(list) ],
 };
