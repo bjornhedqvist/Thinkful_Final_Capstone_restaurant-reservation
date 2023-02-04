9465e6c981289b3d3c6c9639b83888f31611daaa
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-02-03 17:10:39 -0800

    US1 backend: POST /reservations
    ✓ returns 400 if reservation_date is not a date (88 ms)
    ✓ returns 400 if reservation_time is not a time (94 ms)

--- back-end/src/errors/hasProperties.js
+++ back-end/src/errors/hasProperties.js
@@ -1,6 +1,8 @@
 function hasProperties(...properties) {
     return function (req, res, next) {
       const { data = {} } = req.body;
+      const validDateFormat = /\d{4}-\d{2}-\d{2}/g
+      const validTimeFormat = /[0-9]{2}:[0-9]{2}/g
   
       try {
         properties.forEach((property) => {
@@ -10,12 +12,21 @@ function hasProperties(...properties) {
             throw error;
           }
         });
-        console.log(data.people)
+        //validatiors for people, reservation_date, and reservation_time field content
         if(!Number.isInteger(data.people)){
             const error = new Error(`The people field must be a number`)
             error.status = 400
             throw error
-          }
+          }else if(!validDateFormat.test(data.reservation_date)){
+            const error = new Error(`The reservation_date field must be a valid date, ex. 2026-12-30`)
+            error.status = 400
+            throw error
+        }else if(!validTimeFormat.test(data.reservation_time)){
+            const error = new Error(`The reservation_time field must be a valid time, ex. 18:00`)
+            error.status = 400
+            throw error
+        }
+
         next();
       } catch (error) {
         next(error);
